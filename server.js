a179fc8888baf003a0c9ad723b893119055885bb
Jian Earle <37029790+jearle10@users.noreply.github.com>
2021-01-24 13:52:07 +0000

    Update server.js

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(bodyParser.json())
 
 // Instruct the app to listen on port 3000
 
-app.listen(80, () => {
+app.listen(3000, () => {
   console.log(`Server listening on port 3000`); // Call back when server listening
 });
 
