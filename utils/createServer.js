ac21941397adc7c4537e8ef836058cd387ad9355
Earle <jiearle@deloitte.ie>
2021-02-03 16:41:56 +0000

    Fixed typo in createServer file

--- utils/createServer.js
+++ utils/createServer.js
@@ -1,10 +1,10 @@
 const express = require("express");
 
-function createSever() {
+function createServer() {
   let port = process.env.PORT || 3000;
   server = express();
   server.listen(port, () => console.log("Server listening on port 3000"));
   return server;
 }
 
-module.exports = createSever;
+module.exports = createServer;
